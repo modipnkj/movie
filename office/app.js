5c1b75747361160f91562ce8b0ad6db4e7886282
modipnkj <52039422+modipnkj@users.noreply.github.com>
2019-06-20 18:41:13 +0530

    Add files via upload

---
+++ office/app.js
@@ -0,0 +1,22 @@
+ new Vue({
+    el:'#vueapp',
+    data:{
+        movie1:'Harry Potter and the Order of the Phoenix',
+        show: false,
+        show2:false,
+        show3:false,
+        imageLink1: 'https://lh3.googleusercontent.com/OGQkzkYdfhAXE6p-xufS_6mB3MtjDfPM6HZPdDrX5uJ09Dh1vzLZ6YflPIduh2Jk-h7H',
+        movie2:'The Lord of the Rings: The Fellowship of the Ring',
+        imageLink2: 'https://bit.ly/2tC1Lcg',
+        movie3:'Avengers: Endgame',
+        imageLink3: 'https://bit.ly/2Pzczlb'  
+    }
+    
+ });
+ new Vue({
+     el:'#upload',
+     data:{
+         su:false,
+        
+     }
+ })
